62e0e3ab1b97a9c29dda92aa2a6b353c0ed73f1c
Jaimie <162511163+JaimieHemmings@users.noreply.github.com>
2024-03-25 19:16:59 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -80,7 +80,6 @@ async function displayMovieDetails() {
 
     //Overlay for BG Image
     displayBackgroundImage('movie', movie.backdrop_path);
-    console.log(movie.backdrop_path);
 
     const div = document.createElement('div');
     div.innerHTML = `<div class="details-top">
@@ -139,6 +138,67 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 }
 
+//Display Show details
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    //Overlay for BG Image
+    displayBackgroundImage('tv', show.backdrop_path);
+
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="details-top">
+    <div>
+    ${
+        show.poster_path
+        ?
+        `<img 
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top"
+            alt="${show.name}" 
+        />`
+        :
+        `<img 
+            src="../images/no-image.jpg" class="card-img-top"
+            alt="${show.name}" 
+        />`
+    }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${show.first_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => 
+            `<li>${genre.name}</li>`
+            ).join('')}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">No. of Episodes:</span> ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Last Episode to Air:</span> ${show.last_episode_to_air.name}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+    </div>
+  </div>`;
+
+  document.querySelector('#show-details').appendChild(div);
+}
+
 //Display backdrop
 function displayBackgroundImage(type, backgroundPath) {
     const overlayDiv = document.createElement('div');
@@ -160,6 +220,54 @@ function displayBackgroundImage(type, backgroundPath) {
         document.querySelector("#show-details").appendChild(overlayDiv);
     }
 }
+
+//Initiate Swiper
+async function displaySlider() {
+    const { results } = await fetchAPIData('movie/now_playing');
+
+        results.forEach((movie) => {
+            const div = document.createElement('div');
+            div.classList.add('swiper-slide');
+
+            div.innerHTML = `
+                <a href="movie-details.html?id=${movie.id}">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+                </a>
+                <h4 class="swiper-rating">
+                    <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+                </h4>
+            `;
+            
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();   
+    });    
+}
+
+
+function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      freeMode: true,
+      loop: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+        },
+        700: {
+          slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
 
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
@@ -204,6 +312,7 @@ function init() {
         case '/':
         case '/index.html':
             displayPopularMovies();
+            displaySlider();
             break;
         case '/shows.html':
         case '/shows':
@@ -215,7 +324,7 @@ function init() {
             break;
         case '/tv-details':
         case '/tv-details.html':
-            console.log('TV Details');
+            displayShowDetails();
             break;
         case '/search':
         case '/search.html':
